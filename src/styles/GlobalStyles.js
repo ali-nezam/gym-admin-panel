4f4fc474a4c483b11e03e39625af9ffec502f161
ali nezam <ali.nezam.2003@gmail.com>
2025-07-14 00:27:22 +0330

    add vazirmatn font to index html and install in the package.json and  apply that in main .jsx and global.js

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -72,7 +72,8 @@ html {
 }
 
 body {
-  font-family: "Poppins", sans-serif;
+  /* font-family: "Poppins", sans-serif; */
+   font-family: 'Vazirmatn', sans-serif;
   color: var(--color-grey-700);
 
   transition: color 0.3s, background-color 0.3s;
