a819832f57db1d4aa8ac08bdf37cec0d7f0fb6ca
ali nezam <ali.nezam.2003@gmail.com>
2025-07-13 15:36:57 +0330

    update app.jsx for add first rotes code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import AppLayout from "./ui/AppLayout";
+import Coaches from "./pages/Coaches";
+import Dashboard from "./pages/Dashboard";
+import Members from "./pages/Members";
+import Settings from "./pages/Settings";
+import Enrollments from "./pages/Enrollments ";
+
 function App() {
-  return <h1>Vite + React</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route element={<Coaches />} path="coaches" />
+          <Route element={<Dashboard />} path="dashboard" />
+          <Route element={<Enrollments />} path="enrollments" />
+          <Route element={<Members />} path="members" />
+          <Route element={<Settings />} path="settings" />
+        </Route>
+        <Route></Route>/
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
