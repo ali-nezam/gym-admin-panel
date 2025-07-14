b8318babf7c67565532d9df9b4b3358ce2b84698
ali nezam <ali.nezam.2003@gmail.com>
2025-07-14 16:13:37 +0330

    install supabase package & update react-query package and config react-query to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,29 @@ import Members from "./pages/Members";
 import Settings from "./pages/Settings";
 import Enrollments from "./pages/Enrollments ";
 import GlobalStyles from "./styles/GlobalStyles";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
     <>
-      <GlobalStyles />
-      <BrowserRouter>
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route index element={<Navigate replace to="dashboard" />} />
-            <Route element={<Coaches />} path="coaches" />
-            <Route element={<Dashboard />} path="dashboard" />
-            <Route element={<Enrollments />} path="enrollments" />
-            <Route element={<Members />} path="members" />
-            <Route element={<Settings />} path="settings" />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyles />
+        <BrowserRouter>
+          <Routes>
+            <Route element={<AppLayout />}>
+              <Route index element={<Navigate replace to="dashboard" />} />
+              <Route element={<Coaches />} path="coaches" />
+              <Route element={<Dashboard />} path="dashboard" />
+              <Route element={<Enrollments />} path="enrollments" />
+              <Route element={<Members />} path="members" />
+              <Route element={<Settings />} path="settings" />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   );
 }
