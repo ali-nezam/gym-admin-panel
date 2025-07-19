3f595f6c47c483c29011eab2614e9fc639f54cde
ali nezam <ali.nezam.2003@gmail.com>
2025-07-19 21:33:06 +0330

    edit Logo.jsx and use icon and word for Logo and delete image png

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,18 +1,46 @@
+import { Dumbbell } from "lucide-react";
 import styled from "styled-components";
 
 const StyledLogo = styled.div`
-  text-align: center;
-`;
+  margin: 2.4rem auto;
+  margin-bottom: 0;
+  justify-content: center;
+  align-items: center;
+  gap: 1.7rem;
+  display: flex;
+  color: #000;
+  box-shadow: 0 8px 24px rgba(0, 0, 0, 0.03);
+  background-color: #f8f9fa;
+  border-radius: 1.2rem;
 
-const Img = styled.img`
-  width: auto;
-  height: 8.6rem;
+  */ h1 {
+    font-size: 2.6rem;
+    font-weight: 800;
+  }
+  svg {
+    color: #000;
+    width: 4.6rem;
+    height: 4.6rem;
+    background-color: #d3f9d8;
+    padding: 0.8rem;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #37b24d;
+  }
+  p {
+    color: #838383;
+    font-size: 1rem;
+  }
 `;
 
 function Logo() {
   return (
     <StyledLogo>
-      <Img src="src\data\logo\logo-light.png" alt="logo" />
+      <Dumbbell />
+      <h1>GYM ADMIN</h1>
+      <p>v.01</p>
     </StyledLogo>
   );
 }
