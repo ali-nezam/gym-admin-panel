40c388b6d7715a064622693137c14e74e06f2c54
ali nezam <ali.nezam.2003@gmail.com>
2025-07-19 21:20:29 +0330

    create searchBox.jsx

---
+++ src/ui/SearchBox.jsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import { HiOutlineSearch } from "react-icons/hi";
+
+const SearchWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: #fafbff;
+  border-radius: 8px;
+  padding: 0.8rem 1.2rem;
+  width: 21rem;
+  background-color: ${(props) => props.type};
+`;
+
+const SearchIcon = styled(HiOutlineSearch)`
+  font-size: 1.8rem;
+  color: #b5b7c0;
+  margin-left: 0.8rem;
+`;
+
+const SearchInput = styled.input`
+  border: none;
+  outline: none;
+  background: transparent;
+  font-size: 1.4rem;
+  flex: 1;
+  direction: rtl;
+
+  &::placeholder {
+    color: #b5b7c0;
+  }
+`;
+
+function SearchBox({ placeholder = "جستجو...", type }) {
+  return (
+    <SearchWrapper type={type}>
+      <SearchIcon />
+      <SearchInput type="text" placeholder={placeholder} />
+    </SearchWrapper>
+  );
+}
+
+export default SearchBox;
