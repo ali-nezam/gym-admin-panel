f393de5c8323b2134f8b4776b35a8806f09bf6ad
ali nezam <ali.nezam.2003@gmail.com>
2025-07-22 14:25:13 +0330

    create Filter.jsx for use in components for filter content

---
+++ src/ui/Filter.jsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+const StyledFilter = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 0.8rem;
+  padding: 0.6rem 0.6rem;
+  width: fit-content;
+  background-color: #fafbff;
+  border-radius: 8px;
+  /* border: 8px #000 solid; */
+`;
+
+const FilterButton = styled.button`
+  background-color: #fafbff;
+  border: none;
+  color: #b5b7c0;
+  cursor: pointer;
+  padding: 0.4rem 0.9rem;
+  border-radius: 8px;
+  &:hover {
+    color: #fff;
+    background-color: #5932ea;
+  }
+  &:nth-child(2) {
+    color: #fff;
+    background-color: #5932ea;
+  }
+`;
+
+function Filter({ options }) {
+  return (
+    <StyledFilter>
+      {options.map((option) => (
+        <FilterButton>{option}</FilterButton>
+      ))}
+    </StyledFilter>
+  );
+}
+export default Filter;
