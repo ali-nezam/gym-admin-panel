ecb4f9d382dc8fdcc74c72971eb394b1a04cdace
ali nezam <ali.nezam.2003@gmail.com>
2025-07-23 10:08:03 +0330

    config toast setting to show notficatin

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import GlobalStyles from "./styles/GlobalStyles";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import PageNotFound from "./pages/PageNotFound";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   const queryClient = new QueryClient();
@@ -30,6 +31,22 @@ function App() {
           </Routes>
         </BrowserRouter>
         <ReactQueryDevtools initialIsOpen={false} />
+        <Toaster
+          position="top-center"
+          gutter={12}
+          containerStyle={{ margin: "8px" }}
+          toastOptions={{
+            sucssess: { duration: 3000 },
+            error: { duration: 5000 },
+            style: {
+              fontSize: "16px",
+              maxWidth: "500px",
+              padding: "16px 24px",
+              backgroundColor: "var(--color-grey-0)",
+              color: "var(--color-grey-700)",
+            },
+          }}
+        />
       </QueryClientProvider>
     </>
   );
