b70a6775002c3533fa25dfe0602e0484a6d0742d
ali nezam <ali.nezam.2003@gmail.com>
2025-07-26 17:51:16 +0330

    create formatPhoneNumber.js for edit phone number delete zero and Separate with -

---
+++ src/utils/formatPhoneNumber.js
@@ -0,0 +1,19 @@
+export default function formatPhoneNumber(number) {
+  if (!number) return "";
+
+  // حذف تمام کاراکترهای غیرعددی
+  const digits = number.replace(/\D/g, "");
+  // حذف صفر ابتدایی (در صورت وجود)
+  const cleaned = digits.startsWith("0") ? digits.slice(1) : digits;
+
+  // بررسی طول
+  if (cleaned.length !== 10) return "شماره نامعتبر است";
+
+  // اعمال فرمت
+  return `${cleaned.slice(0, 3)} - ${cleaned.slice(3, 6)} - ${cleaned.slice(
+    6
+  )}`;
+  // console.log(
+  //   `${cleaned.slice(0, 3)} - ${cleaned.slice(3, 6)} - ${cleaned.slice(6)}`
+  // );
+}
