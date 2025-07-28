f56e83b55716d487ab43b36670d29f40015c68ff
ali nezam <ali.nezam.2003@gmail.com>
2025-07-29 00:30:05 +0330

    create minSpiner

---
+++ src/ui/SpinnerMini.jsx
@@ -0,0 +1,49 @@
+import styled, { keyframes } from "styled-components";
+import { Dumbbell } from "lucide-react";
+
+const bounce = keyframes`
+  0%, 100% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-12px);
+  }
+`;
+
+const Overlay = styled.div`
+  /* position: fixed; */
+  inset: 0;
+  /* background-color: rgba(255, 255, 255, 0.95); */
+  z-index: 9999;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const SpinnerIcon = styled(Dumbbell)`
+  /* width: 4rem; */
+  /* height: 4rem; */
+  width: 3.2rem;
+  height: 3.2rem;
+  stroke: #5932ea;
+  background-color: #ffffff;
+  animation: ${bounce} 1s infinite ease-in-out;
+`;
+
+const LoadingText = styled.p`
+  margin-top: 1rem;
+  font-size: 1.4rem;
+  color: #555;
+`;
+
+function SpinnerMini({ text = "در حال بارگذاری..." }) {
+  return (
+    <Overlay>
+      <SpinnerIcon />
+      <LoadingText>{text}</LoadingText>
+    </Overlay>
+  );
+}
+
+export default SpinnerMini;
