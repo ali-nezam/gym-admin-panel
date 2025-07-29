2ec847ebf444f0334b0cc974c81c55724cb0c301
ali nezam <ali.nezam.2003@gmail.com>
2025-07-29 12:46:35 +0330

    create Form.jsx as compound component to yse generic in folder compound

---
+++ src/Compound component/Form.jsx
@@ -0,0 +1,60 @@
+import { forwardRef } from "react";
+import styled from "styled-components";
+
+function Form({ children, onSubmit }) {
+  return <form onSubmit={onSubmit}>{children}</form>;
+}
+
+Form.Label = function FormLabel({ children, htmlFor }) {
+  return <Label htmlFor={htmlFor}>{children}</Label>;
+};
+
+Form.Input = function FormInput(props, ref) {
+  const { error, ...rest } = props;
+  return (
+    <>
+      <Input ref={ref} {...rest} />
+      {error?.message && <ErrorMessage>{error?.message}</ErrorMessage>}
+    </>
+  );
+};
+
+Form.Input = forwardRef(Form.Input);
+
+Form.Select = function FormSelect(props, ref) {
+  const { error, children, ...rest } = props;
+  return (
+    <>
+      <Select ref={ref} {...rest}>
+        {children}
+      </Select>
+      ;{error?.message && <ErrorMessage>{error?.message}</ErrorMessage>}
+    </>
+  );
+};
+Form.Select = forwardRef(Form.Select);
+
+export default Form;
+
+const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  font-size: 1.4rem;
+`;
+const Input = styled.input`
+  padding: 0.8rem;
+  font-size: 1.2rem;
+  border-radius: 0.6rem;
+  border: 1px solid #ccc;
+`;
+
+const ErrorMessage = styled.span`
+  color: red;
+`;
+
+const Select = styled.select`
+  padding: 0.8rem;
+  font-size: 1.2rem;
+  border-radius: 0.6rem;
+  border: 1px solid #ccc;
+`;
